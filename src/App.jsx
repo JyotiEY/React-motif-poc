f8b0d06632a88f829bbed8166a5b61ca9ba19bd0
Jyoti Sharma <jyoti.sharma18@in.ey.com>
2024-08-22 10:57:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Dashboard } from "./pages";
+
+const App = () => {
+  return (
+    <>
+      <h1>React Motif</h1>
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from './login/login';
+import Dashboard from './dashboard/dashboard';
+
+export {
+    Login,
+    Dashboard
+}
